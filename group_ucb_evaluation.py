29a8087ebc4279cf69149cf3f35cf491df333b85
Aakash Krishna GS <aakash3697@gmail.com>
2022-09-29 17:26:38 +0000

    random seed via CLAs

--- group_ucb_evaluation.py
+++ group_ucb_evaluation.py
@@ -5,6 +5,7 @@ import torch
 
 from policy_selection.ucb import GroupedUCB
 from ppo import PPO
+import sys
 
 
 def get_policies(policies=None, policy_dir="policy_library_20220820/**.pth", rm_env_diversity=False):
@@ -53,10 +54,11 @@ def remove_env_diversity(policies):
 
 def run_group_ucb(group_config, policy_locs, init_policies, rho=2,
                   eval_duration=30, epochs=100, pickup_from=None,
-                  use_dummy_arms=False):
+                  use_dummy_arms=False, random_seed=None):
     ucb = GroupedUCB(group_config, policy_locs, init_policies,
                      log_dir=f"data/group_ucb_log_data/",
-                     pickup_from=pickup_from, use_dummy_arms=use_dummy_arms)
+                     pickup_from=pickup_from, use_dummy_arms=use_dummy_arms,
+                     random_seed=random_seed)
     ucb.run_ucb(epochs=epochs, rho=rho, eval_duration=eval_duration)
 
 
@@ -90,7 +92,25 @@ if __name__ == "__main__":
         all_group_policies += group_config["group_policies"][group]
 
     policy_locs, init_policies = get_policies(policies=all_group_policies, rm_env_diversity=False)
-
+    # print(int(sys.argv[1])
+    seed = int(sys.argv[1])
+    pickup = int(sys.argv[2])
     run_group_ucb(group_config, policy_locs, init_policies,
-                  rho=2, eval_duration=30, epochs=None,
-                  use_dummy_arms=False)
+                  rho=2, eval_duration=30, epochs=1000,
+                  use_dummy_arms=False, random_seed=seed,
+                  pickup_from=pickup)
+
+"""
+nohup python group_ucb_evaluation.py 1337> data/group_ucb_std_data/random_seed_1337.out &
+nohup python group_ucb_evaluation.py 31720 13 > data/group_ucb_std_data/random_seed_31720.out &
+nohup python group_ucb_evaluation.py 21538 14 > data/group_ucb_std_data/random_seed_21538.out &
+nohup python group_ucb_evaluation.py 69175 15 > data/group_ucb_std_data/random_seed_69175.out &
+nohup python group_ucb_evaluation.py 17170 16 > data/group_ucb_std_data/random_seed_17170.out &
+nohup python group_ucb_evaluation.py 36621 17 > data/group_ucb_std_data/random_seed_36621.out &
+nohup python group_ucb_evaluation.py 41408 18 > data/group_ucb_std_data/random_seed_41408.out &
+nohup python group_ucb_evaluation.py 22808 19 > data/group_ucb_std_data/random_seed_22808.out &
+nohup python group_ucb_evaluation.py 19197 20 > data/group_ucb_std_data/random_seed_19197.out &
+nohup python group_ucb_evaluation.py 23509 21 > data/group_ucb_std_data/random_seed_23509.out &
+nohup python group_ucb_evaluation.py 69266 22 > data/group_ucb_std_data/random_seed_69266.out &
+nohup python group_ucb_evaluation.py 36621 23 > data/group_ucb_std_data/random_seed_36621.out &
+"""
